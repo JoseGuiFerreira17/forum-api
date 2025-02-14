b6e1d2fcdde97534d58425dc886f2e98d1a16cbe
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-14 14:30:33 -0300

    Controller: answer question

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Param } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+import { UserPayload } from '@/infra/auth/jwt_strategy';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
+import { z } from 'zod';
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question';
+
+const answerQuestionBodySchema = z.object({
+  content: z.string().min(3),
+});
+
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema);
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body(bodyValidationPipe) body: AnswerQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body;
+
+    await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId: user.sub,
+      attachmentsIds: [],
+    });
+  }
+}
