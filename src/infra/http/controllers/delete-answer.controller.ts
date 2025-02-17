31042f182995e955c81ea4ffcc8065c0cba5d6f0
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-17 14:07:03 -0300

    Controller: delete answer

---
+++ src/infra/http/controllers/delete-answer.controller.ts
@@ -0,0 +1,23 @@
+import { Delete, Param } from '@nestjs/common';
+import { Controller, HttpCode } from '@nestjs/common';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+import { UserPayload } from '@/infra/auth/jwt_strategy';
+
+import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer';
+
+@Controller('/answers/:id')
+export class DeleteAnswerController {
+  constructor(private deleteAnswer: DeleteAnswerUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') answerId: string,
+  ) {
+    await this.deleteAnswer.execute({
+      authorId: user.sub,
+      answerId,
+    });
+  }
+}
